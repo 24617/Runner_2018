cc8cfdbb5a52fd4fbe615c19611d5436b6798f0f
Alexbroking <alexbroking@gmail.com>
2018-09-28 13:34:20 +0200

    StartOfGame

---
+++ Endless_Runner_2018/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    
+    public Transform Player;
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+        transform.position = new Vector3(Player.position.x + 6,transform.position.y,transform.position.z);
+	}
+}
---
+++ Endless_Runner_2018/Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float[] Lanes;
+    
+    public int Lane = 0;
+    bool StartGame = false;
+
+
+    
+    void Start () {
+      
+    }
+
+
+    void Update() {
+
+        if (Input.GetKeyDown("space"))
+        {
+            StartGame = true;
+        }
+
+        if (StartGame == true)
+        {
+            transform.position += Vector3.right * Time.deltaTime * 5;
+            
+        }
+        
+        transform.position = new Vector3(transform.position.x, transform.position.y, Lanes[Lane]);
+        
+
+        if (Input.GetKeyDown("up"))
+        {
+            if (Lane == 2 || Lane == 1 || Lane == 0)
+            {
+                Lane += 1;
+               
+            }
+                
+        }
+
+        if (Input.GetKeyDown("down"))
+        {
+            if (Lane == 1 || Lane == 2 || Lane == 3)
+            {
+                Lane -= 1;
+            }
+
+        }
+
+    }
+}
